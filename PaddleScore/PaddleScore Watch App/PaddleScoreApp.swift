721c1484ebab1b35a79bde646cd72aa9bfba9a07
Yanet Rodriguez <yanet-rodriguez@outlook.com.ar>
2024-10-09 12:43:47 -0300

    Add Firebase

--- PaddleScore/PaddleScore Watch App/PaddleScoreApp.swift
+++ PaddleScore/PaddleScore Watch App/PaddleScoreApp.swift
@@ -6,12 +6,21 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import WatchKit
 
 @main
 struct PaddleScore_Watch_AppApp: App {
+    @WKExtensionDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, WKExtensionDelegate {
+    func applicationDidFinishLaunching() {
+        FirebaseApp.configure()
     }
 }
